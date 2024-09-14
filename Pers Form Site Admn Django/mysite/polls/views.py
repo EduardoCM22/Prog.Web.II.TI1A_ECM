e1ad19e8220804e88c54ab364094642cee7fdbde
edwcm <none>
2024-09-13 22:28:28 -0600

    Personalice la lista de cambios del sitio administrativo

--- Pers Form Site Admn Django/mysite/polls/views.py
+++ Pers Form Site Admn Django/mysite/polls/views.py
@@ -11,8 +11,11 @@ from django.views import generic
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
