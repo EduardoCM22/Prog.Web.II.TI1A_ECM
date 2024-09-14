b7e2c4f5bd984cc89355b911f94fbc9897fb415d
edwcm <none>
2024-09-13 23:03:45 -0600

    Jugando con la API, Nombre de Question en Change Question,DjangoP2

--- Pers Form Site Admn Django/mysite/polls/models.py
+++ Pers Form Site Admn Django/mysite/polls/models.py
@@ -7,6 +7,8 @@ import datetime
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
     @admin.display(
         boolean=True,
         ordering="pub_date",
@@ -20,3 +22,5 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
